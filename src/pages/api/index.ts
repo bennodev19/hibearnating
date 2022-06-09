8335f53b95ae3e49ab3c045e8020ac88889951bf
benno <bennokohrs@gmail.com>
2022-06-09 19:07:51 +0200

    vercel trial and error (Buffer won't work :/)

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,6 +1,6 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-// import sharp from 'sharp';
-import {Buffer} from "buffer";
+import sharp from 'sharp';
+// import {Buffer} from "buffer";
 import axios from 'axios';
 import {appConfig} from "../../config";
 
@@ -36,20 +36,20 @@ export default async function handler(
         const frozenBuffer = frozenBufferResponse.data as Buffer;
 
         // Blend frozen image with provided image
-        // const output = await sharp(imageBuffer)
-        //     .png()
-        //     .resize(2048, 2048) // needs to be the exact size of the 'frozen.png'
-        //     .composite([
-        //         {
-        //             input: frozenBuffer,
-        //             blend: 'hard-light',
-        //         },
-        //     ]);
-        // const buffer = await output.toBuffer();
-        //
-        // res.send({
-        //     buffer: buffer.toString('base64')
-        // });
+        const output = await sharp()
+            .png()
+            .resize(2048, 2048) // needs to be the exact size of the 'frozen.png'
+            .composite([
+                {
+                    input: frozenBuffer,
+                    blend: 'hard-light',
+                },
+            ]);
+        const buffer = await output.toBuffer();
+
+        res.send({
+            buffer: buffer.toString('base64')
+        });
     } catch (e) {
         console.log("Error", e)
         res.status(400).json({message: 'Something went wrong', e});
