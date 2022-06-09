cc61803206a947f0c6d20351d7ec77f4afe46592
benno <bennokohrs@gmail.com>
2022-06-09 18:48:53 +0200

    vercel trial and error

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -24,36 +24,36 @@ export default async function handler(
         res.status(405).json({message: "Only POST requests allowed!"});
     }
 
-    try {
-        const content = req.body;
-        const imageBuffer = Buffer.from(content.buffer, 'base64');
-
-        // Fetch frozen asset from 'public' folder (Note relative path doesn't work)
-        // const frozenBufferResponse = await axios({
-        //     url: `${appConfig.imageBaseUrl}/frozen.png`,
-        //     responseType: 'arraybuffer'
-        // });
-        // const frozenBuffer = frozenBufferResponse.data as Buffer;
-
-        // Blend frozen image with provided image
-        // const output = await sharp(imageBuffer)
-        //     .png()
-        //     .resize(2048, 2048) // needs to be the exact size of the 'frozen.png'
-        //     .composite([
-        //         {
-        //             input: frozenBuffer,
-        //             blend: 'hard-light',
-        //         },
-        //     ]);
-        // const buffer = await output.toBuffer();
-        //
-        // res.send({
-        //     buffer: buffer.toString('base64')
-        // });
-    } catch (e) {
-        console.log("Error", e)
-        res.status(400).json({message: 'Something went wrong', e});
-    }
+    // try {
+    //     const content = req.body;
+    //     const imageBuffer = Buffer.from(content.buffer, 'base64');
+    //
+    //     // Fetch frozen asset from 'public' folder (Note relative path doesn't work)
+    //     // const frozenBufferResponse = await axios({
+    //     //     url: `${appConfig.imageBaseUrl}/frozen.png`,
+    //     //     responseType: 'arraybuffer'
+    //     // });
+    //     // const frozenBuffer = frozenBufferResponse.data as Buffer;
+    //
+    //     // Blend frozen image with provided image
+    //     // const output = await sharp(imageBuffer)
+    //     //     .png()
+    //     //     .resize(2048, 2048) // needs to be the exact size of the 'frozen.png'
+    //     //     .composite([
+    //     //         {
+    //     //             input: frozenBuffer,
+    //     //             blend: 'hard-light',
+    //     //         },
+    //     //     ]);
+    //     // const buffer = await output.toBuffer();
+    //     //
+    //     // res.send({
+    //     //     buffer: buffer.toString('base64')
+    //     // });
+    // } catch (e) {
+    //     console.log("Error", e)
+    //     res.status(400).json({message: 'Something went wrong', e});
+    // }
 }
 
 // https://stackoverflow.com/questions/53550932/dotenv-values-not-loaded-in-nextjs
